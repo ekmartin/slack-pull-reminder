770fdb0cde4d61af4e8e8393a7074704eeec12be
Martin Ek <mail@ekmartin.com>
2016-04-04 17:46:13 +0200

    Add setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('requirements.txt', 'r') as fd:
+    requirements = fd.read().strip().split('\n')
+
+setup(
+    name='slack-pull-reminder',
+    version='0.1.0',
+    url='http://github.com/ekmartin/slack-pull-reminder',
+    author='Martin Ek',
+    author_email='mail@ekmartin.com',
+    description='Posts a Slack reminder with a list of open pull requests for an organization',
+    py_modules=['slack_pull_reminder'],
+    license='MIT',
+    install_requires=requirements,
+    entry_points='''
+        [console_scripts]
+        slack_pull_reminder=slack_pull_reminder:cli
+    '''
+)
