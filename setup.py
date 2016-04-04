2d9b1242111086e57a461daae5f1a5e4afbf2c74
Martin Ek <mail@ekmartin.com>
2016-04-04 17:56:19 +0200

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup
 
+with open('README.rst', 'rb') as f:
+    readme = f.read().decode('utf-8')
+
 with open('requirements.txt', 'r') as fd:
     requirements = fd.read().strip().split('\n')
 
@@ -10,6 +13,7 @@ setup(
     author='Martin Ek',
     author_email='mail@ekmartin.com',
     description='Posts a Slack reminder with a list of open pull requests for an organization',
+    long_description=readme,
     py_modules=['slack_pull_reminder'],
     license='MIT',
     install_requires=requirements,
