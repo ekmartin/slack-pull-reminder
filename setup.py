c513d3c3fe28bf888eb56e20696917ed593d2a0c
Doppins <contact@doppins.com>
2018-06-12 14:46:39 +0000

    Upgrade dependency requests to ==2.19.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     py_modules=['slack_pull_reminder'],
     license='MIT',
     install_requires=[
-        'requests==2.18.4',
+        'requests==2.19.0',
         'github3.py==1.0.0a4'
     ],
     entry_points='''
