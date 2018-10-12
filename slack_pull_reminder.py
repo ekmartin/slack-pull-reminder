d5f895524f626010504e294330b76825027c2ed7
Mark Davidoff <markddavidoff@users.noreply.github.com>
2018-10-12 13:19:30 -0700

    filter repositories you check using a comma sep REPO env var

--- slack_pull_reminder.py
+++ slack_pull_reminder.py
@@ -8,6 +8,10 @@ POST_URL = 'https://slack.com/api/chat.postMessage'
 
 ignore = os.environ.get('IGNORE_WORDS')
 IGNORE_WORDS = ignore.split(',') if ignore else []
+
+repos = os.environ.get('REPOS')
+REPOS = repos.split(',') if repos else []
+
 SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL', '#general')
 
 try:
@@ -61,6 +65,9 @@ def fetch_organization_pulls(organization_name):
     lines = []
 
     for repository in organization.repositories():
+        if REPOS and repository.name.lower() not in REPOS:
+            # if repos list is defined, skip repos not defined in filter
+            continue
         unchecked_pulls = fetch_repository_pulls(repository)
         lines += format_pull_requests(unchecked_pulls, organization_name,
                                       repository.name)
