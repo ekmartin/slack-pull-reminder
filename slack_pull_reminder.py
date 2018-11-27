88ba5c11a3709ab06e6b9c5ba96dc1dad8336109
Mark Davidoff <markddavidoff@users.noreply.github.com>
2018-11-27 08:52:48 -0800

    make our own combined build status as github's doesn't really do what we want

--- slack_pull_reminder.py
+++ slack_pull_reminder.py
@@ -1,8 +1,12 @@
 import os
 import sys
+from collections import namedtuple
 
 import requests
-from github3 import login, pulls, repos
+from github3 import login
+from github3.repos import status
+
+CombinedBuildStatus = namedtuple('CombinedBuildStatus', ('success', 'pending', 'failure'))
 
 POST_URL = 'https://slack.com/api/chat.postMessage'
 
@@ -23,9 +27,10 @@ PENDING_EMOJI = os.environ.get('PENDING_EMOJI', u'⟳')
 FAILURE_EMOJI = os.environ.get('FAILURE_EMOJI', u'⨉')
 
 state_to_emoji = {
-    'success': SUCCESS_EMOJI,
-    'pending': PENDING_EMOJI,
-    'failure': FAILURE_EMOJI,
+    CombinedBuildStatus.success: SUCCESS_EMOJI,
+    CombinedBuildStatus.pending: PENDING_EMOJI,
+    CombinedBuildStatus.failure: FAILURE_EMOJI,
+    None: ' '  # for when there is no status for one PR but you still want the rest of the text to line up
 }
 
 try:
@@ -60,15 +65,63 @@ def is_valid_title(title):
     return True
 
 
-def get_combined_status(pull_request):
+def fetch_combined_build_status(pull_request):
     """
-    A fun hack :/ to get the combined status of a pull request. github3.py doesn't support this atm, so hack it in
+    Can't use the github api pull request combined statuses endpoint as for some reason it combines review statuses
+    and build statuses and we only want build statuses
     :param pull_request: github3.py PullRequest obj
-    :return: github3.py CombinedStatus obj
+    :return: a CombinedBuildStatus enum value or None if no statuses
     """
-    combined_status_url = pull_request.statuses_url.replace('/statuses/', '/status/')
-    json = pull_request._json(pull_request._get(combined_status_url), 200)
-    return pull_request._instance_or_null(repos.status.CombinedStatus, json)
+    build_statuses = fetch_pull_request_build_statuses(pull_request)
+
+    # { status.context: most recently updated Status with that context}
+    most_recent_status_by_context = {}
+
+    for build_status in build_statuses:
+        if build_status.updated_at is None:
+            # gihub3.py seems to imply this can be None, so ignore those statuses
+            continue
+
+        if build_status.context not in most_recent_status_by_context:
+            most_recent_status_by_context[build_status.context] = build_status
+        elif build_status.updated_at > most_recent_status_by_context[build_status.context].updated_at:
+            most_recent_status_by_context[build_status.context] = build_status
+
+    most_recent_statuses = most_recent_status_by_context.values()
+
+    if len(most_recent_statuses) == 0:
+        # no statuses, return None
+        return None
+
+    any_pending = False
+    for status in most_recent_statuses:
+        if status.state == CombinedBuildStatus.failure:
+            # if any failed, return overall failure
+            return CombinedBuildStatus.failure
+        if status.state == CombinedBuildStatus.pending:
+            # can't return here in case there's a failed one after it
+            any_pending = True
+    return CombinedBuildStatus.pending if any_pending else CombinedBuildStatus.success
+
+
+def fetch_pull_request_build_statuses(pull_request):
+    """
+    # TODO Remove after the PR adding this is merged into github3.py
+    # PR: https://github.com/sigmavirus24/github3.py/pull/896
+    Return iterator of all Statuses associated with head of this pull request.
+
+     :param pull_request: PullRequest object
+    :returns:
+        generator of statuses for this pull request
+    :rtype:
+        :class:`~github3.repos.Status`
+    """
+    if pull_request.repository is None:
+        return []
+    url = pull_request._build_url(
+        'statuses', pull_request.head.sha, base_url=pull_request.repository._api
+    )
+    return pull_request._iter(-1, url, status.Status)
 
 
 def format_pull_requests(pull_requests, owner, repository):
@@ -77,9 +130,9 @@ def format_pull_requests(pull_requests, owner, repository):
     for pull in pull_requests:
         if is_valid_title(pull.title):
             creator = pull.user.login
-            combined_status = get_combined_status(pull)
+            combined_status = fetch_combined_build_status(pull)
             if SHOW_BUILD_STATUS and combined_status:
-                build_status = state_to_emoji.get(combined_status.state, " ")
+                build_status = state_to_emoji.get(combined_status)
             else:
                 build_status = ""
             line = '*{}[{}/{}]* <{}|{} - by {}>'.format(
@@ -127,6 +180,7 @@ def cli():
     if lines:
         text = INITIAL_MESSAGE + '\n'.join(lines)
         send_to_slack(text)
+
 
 if __name__ == '__main__':
     cli()
