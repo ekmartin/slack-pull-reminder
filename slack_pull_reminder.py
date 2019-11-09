374c9b972d66200577b8b9744acba3b0df6ff363
Saul Ortigoza <15851880+sortigoza@users.noreply.github.com>
2019-11-09 15:58:43 -0800

    second refactor iteration including filtering features (#1)
    * sortigoza/print-to-terminal
    
    * refactored output formats
    
    * filters working
    
    * renamed main
    
    * include age and mergeable
    
    * pull extra fields

--- slack_pull_reminder.py
+++ slack_pull_reminder.py
@@ -1,7 +1,8 @@
 import os
 import logging
-from pprint import pformat
+from pprint import pformat, pprint
 from collections import namedtuple
+from datetime import datetime, timezone
 
 import requests
 from github3 import login
@@ -19,6 +20,9 @@ class Config:
         self._load_slack_configs()
         self._load_github_configs()
         self.LOGLEVEL = os.environ.get("LOGLEVEL", logging.INFO)
+        logger.setLevel(self.LOGLEVEL)
+
+        logger.debug(vars(self))
 
     def _load_slack_configs(self):
         self.SLACK_API_TOKEN = os.environ.get("SLACK_API_TOKEN", "")
@@ -45,10 +49,13 @@ class Config:
         except KeyError as error:
             raise ConfigError("please set the environment variable %s", str(error))
 
-        ignore = os.environ.get("IGNORE_WORDS")
-        self.IGNORE_WORDS = (
-            [i.lower().strip() for i in ignore.split(",")] if ignore else []
-        )
+        ignore = os.environ.get("IGNORE_TITLE_WORDS", "")
+        self.IGNORE_TITLE_WORDS = [i.lower().strip() for i in ignore.split(",") if i]
+
+        include_labels = os.environ.get("INCLUDE_LABELS", "")
+        self.INCLUDE_LABELS = [
+            i.lower().strip() for i in include_labels.split(",") if i
+        ]
 
         repositories = os.environ.get("REPOSITORIES")
         self.REPOSITORIES = (
@@ -60,9 +67,13 @@ class Config:
             [u.lower().strip() for u in usernames.split(",")] if usernames else []
         )
 
+    def is_stdout_configured(self):
+        return True
+
 
 PullRequest = namedtuple(
-    "PullRequest", "repository_name creator url title pull_requests"
+    "PullRequest",
+    "repository_name creator url title age_hrs labels mergeable review_status pull_requests",
 )
 
 
@@ -116,13 +127,19 @@ class GitHubDataProvider:
                     creator=pull.user.login,
                     url=pull.html_url,
                     title=pull.title,
+                    labels=self._get_pr_labels(pull),
+                    age_hrs=self._get_pr_age(pull),
+                    mergeable=pull.mergeable,
+                    review_status=self._get_pr_review_statuses(pull),
                     pull_requests=None,
                 )
                 for pull in open_pull_requests
             ]
 
         open_pull_requests = [
-            pull for pull in repository.pull_requests() if pull.state == "open"
+            repository.pull_request(pull.number)
+            for pull in repository.pull_requests()
+            if pull.state == "open"
         ]
 
         logger.info(
@@ -143,6 +160,24 @@ class GitHubDataProvider:
             ]
         )
 
+    def _get_pr_labels(self, pull):
+        return [x["name"] for x in pull.labels]
+
+    def _get_pr_age(self, pull):
+        td = datetime.now(timezone.utc) - pull.updated_at
+        hours, _ = divmod(int(td.total_seconds()), 3600)
+        return hours
+
+    def _get_pr_review_statuses(self, pull):
+        review_statuses = {}
+
+        for review in pull.reviews():
+            if review.state not in review_statuses:
+                review_statuses[review.state] = []
+            review_statuses[review.state].append(review.user.login)
+
+        return review_statuses
+
     def _get_repo_names(self, repos):
         return [repo.name for repo in repos]
 
@@ -150,27 +185,35 @@ class GitHubDataProvider:
         return [pull.title for pull in pull_requests]
 
 
-class Formater:
+class PRFilter:
     def __init__(self, config):
         self._config = config
 
-    def format_message_lines(self, open_prs):
-        return [
-            self._format_pull_request(pull, owner=self._config.GITHUB_ORGANIZATION)
-            for pull in open_prs
-            if self._is_valid_title(pull.title)
-        ]
+    def filter(self, pull):
+        return self.is_valid_title(pull) and self.is_valid_labels(pull)
 
-    def _format_pull_request(self, pull, owner=""):
-        return f"*[{owner}/{pull.repository_name}]* <{pull.url}|{pull.title} - by {pull.creator}>"
+    def is_valid_title(self, pull):
+        if not self._config.IGNORE_TITLE_WORDS:
+            return True
 
-    def _is_valid_title(self, title):
-        lowercase_title = title.lower()
-        for ignored_word in self._config.IGNORE_WORDS:
+        lowercase_title = pull.title.lower()
+        for ignored_word in self._config.IGNORE_TITLE_WORDS:
             if ignored_word in lowercase_title:
                 return False
 
         return True
+
+    def is_valid_labels(self, pull):
+        if not self._config.INCLUDE_LABELS:
+            return True
+
+        for label in pull.labels:
+            lowercase_label = label.lower()
+            for filtered_label in self._config.INCLUDE_LABELS:
+                if filtered_label in lowercase_label:
+                    return True
+
+        return False
 
 
 class SlackError(Exception):
@@ -181,7 +224,12 @@ class Slack:
     def __init__(self, config):
         self._config = config
 
-    def send(self, text):
+    def send(self, pull_requests):
+        lines = self.format_message_lines(pull_requests)
+
+        text = self._config.SLACK_INITIAL_MESSAGE + "\n".join(lines)
+        logger.info("slack message:\n%s", text)
+
         payload = {
             "token": self._config.SLACK_API_TOKEN,
             "channel": self._config.SLACK_CHANNEL,
@@ -197,31 +245,76 @@ class Slack:
         if not answer["ok"]:
             raise SlackError(answer["error"])
 
+    def format_message_lines(self, open_prs):
+        return [
+            self._format_pull_request(pull, owner=self._config.GITHUB_ORGANIZATION)
+            for pull in open_prs
+        ]
 
-def cli():
+    def _format_pull_request(self, pull, owner=""):
+        return f"*[{owner}/{pull.repository_name}]* <{pull.url}|{pull.title} - by {pull.creator}>"
+
+
+class StdoutPrint:
+    def __init__(self, config):
+        self._config = config
+
+    def send(self, pull_requests):
+        from pytablewriter import UnicodeTableWriter
+
+        value_matrix_nested = self.format_pr_values(pull_requests)
+        value_matrix = [line for lines in value_matrix_nested for line in lines]
+
+        writer = UnicodeTableWriter()
+        writer.table_name = "open pull request to review"
+        writer.headers = ["repository_name", "[pull.creator] pull.title / pull.url"]
+        writer.value_matrix = value_matrix
+        writer.margin = 1
+
+        writer.write_table()
+
+    def format_pr_values(self, open_prs):
+        return [
+            self._format_pull_request(pull, owner=self._config.GITHUB_ORGANIZATION)
+            for pull in open_prs
+        ]
+
+    def _format_pull_request(self, pull, owner=""):
+        return [
+            [f"{owner}/{pull.repository_name}", f"[{pull.creator}] {pull.title}"],
+            ["", pull.url],
+        ]
+
+
+def main():
     config = Config()
-    logger.setLevel(config.LOGLEVEL)
     github = GitHubDataProvider(config)
     slack = Slack(config)
-    formater = Formater(config)
+    stdout_print = StdoutPrint(config)
+    pr_filter = PRFilter(config)
 
-    lines = formater.format_message_lines(github.fetch_organization_pulls())
+    pull_requests = github.fetch_organization_pulls()
 
-    if not lines:
+    pull_requests_filtered = [pr for pr in pull_requests if pr_filter.filter(pr)]
+
+    if not pull_requests_filtered:
+        logger.warning("no pull requests to send")
         return
 
-    text = config.SLACK_INITIAL_MESSAGE + "\n".join(lines)
-    logger.info("slack message:\n%s", text)
+    pprint(pull_requests_filtered)
+
+    if config.is_stdout_configured():
+        stdout_print.send(pull_requests_filtered)
 
     if config.is_slack_configured():
-        slack.send(text)
+        slack.send(pull_requests_filtered)
     else:
         logger.warning("slack is not configured, message not sent")
 
 
 if __name__ == "__main__":
     try:
-        cli()
+        main()
     except ConfigError as err:
         logger.error(err)
         exit(1)
